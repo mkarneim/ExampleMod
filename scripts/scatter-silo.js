c437bb07dafba7c7cd36bc93b2fc5bad7ab961d0
Michael Karneim <mkarneim@googlemail.com>
2020-01-05 13:44:01 +0100

    Update scatter-silo.js

--- scripts/scatter-silo.js
+++ scripts/scatter-silo.js
@@ -1,7 +1,7 @@
 //this is NOT the complete definition for this block! see content/blocks/scatter-silo.hjson for the stats and other properties.
 
 //create a simple shockwave effect
-const siloLaunchEffect = newEffect(20, e => {
+const siloLaunchEffect = newEffect(200, e => {
     Draw.color(Color.white, Color.lightGray, e.fin()); //color goes from white to light gray
     Lines.stroke(e.fout() * 3); //line thickness goes from 3 to 0
     Lines.circle(e.x, e.y, e.fin() * 100); //draw a circle whose radius goes from 0 to 100
